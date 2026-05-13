92f15c77f662500b987af2ff07adc7abd6a4fce1
Peter Gloag <petergloag@gmail.com>
2026-05-12 22:52:18 -0700

    Yeah... I just did that B)

--- main.cpp
+++ main.cpp
@@ -3,55 +3,20 @@
 #include <sstream>
 #include <string>
 
-void printJSON(JSON j);
-
-void printVal(JSON j)
-{
-    if(j.j_type == j.j_float)
-        printf("%0.2f", j.f());
-    else if(j.j_type == j.j_string)
-        printf("\"%s\"", j.s().c_str());
-    else if(j.j_type == j.j_list)
-    {
-        printf("[ ");
-        for(int i = 0; i < j.l().size(); i++)
-        {
-            // printf("      ");
-            printVal(j.l()[i]);
-            printf(", ");
-        }
-        printf("]");
-    }
-    else if(j.j_type == j.j_json)
-    {
-        printJSON(j);
-    }
-}
-void printJSON(JSON j)
-{
-    printf("{ ");
-    for(auto i : j.json)
-    {
-        printf("%s: ", i.first.c_str());
-        printVal(i.second);
-        printf(", ");
-    }
-    printf("}");
-}
-
 int main(int argc, char* argv[])
 {
+
     JSON j;
 
     if(argc == 1)
-        j.parse("{ \" list \" : [ 1 , 2 , 3 ] }");
+        j.parse("{\"key\":\"val\"}");
 
     if(argc == 2)
     {
         j.parse_from_file(argv[1]);
     }
 
-    printVal(j);
+    j.print();
 
     return 0;
 }
